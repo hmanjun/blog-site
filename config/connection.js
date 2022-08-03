0e83c39d6689f5327798949d2166ebcbe28a0874
hmanjun <harshith.hm03@gmail.com>
2022-08-02 17:35:55 -0700

    Add connection to sql database

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize')
+require('dotenv').config()
+
+let sequelize
+
+if(process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+    sequelize = new Sequelize(
+        process.env.db_name,
+        process.env.db_user,
+        process.env.db_pass,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        }
+    )
+}
+
+module.exports = sequelize
