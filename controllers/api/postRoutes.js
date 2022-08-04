5b59a5cf6e4aa2b6459fdbe6d0febc4aaac6bbd4
hmanjun <harshith.hm03@gmail.com>
2022-08-03 22:50:35 -0700

    Add route listener for creating, updating, and deleting a post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,3 +1,50 @@
 const router = require('express').Router()
+const {Post} = require('../../models')
+
+router.post('/', async (req,res) => {
+    try {
+        const postData = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id
+        })
+        
+        res.status(200).json(postData)
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+router.put('/:id', async (req,res) => {
+    try {
+        const postData = await Post.update(req.body,{
+            where: {id: req.params.id}
+        })
+
+        if(!postData){
+            res.status(400).json({message: `Could not find post with that id`})
+            return
+        }
+        res.status(200).json(postData)
+    } catch (error) {
+        res.status(400).json(err)
+    }
+})
+
+router.delete('/:id', async (req,res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {id: res.body.params}
+        })
+
+        if(!postData){
+            res.status(400).json({message: `Could not find post with that id`})
+            return
+        }
+
+        res.status(200).json(postData)
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
 
 module.exports = router
