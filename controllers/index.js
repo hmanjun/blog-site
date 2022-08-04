74aa534ceddfbe8a673715236aadb6f89704b090
hmanjun <harshith.hm03@gmail.com>
2022-08-03 20:13:25 -0700

    Add route splitter for requests through /api

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,9 @@ const router = require('express').Router()
 const homeRoutes = require('./homeRoutes')
 const apiRoutes = require('./api')
 const dashboardRoutes = require('./dashboardRoutes')
-const loginRoutes = require('./loginRoutes')
 
 router.use('/', homeRoutes)
 router.use('/api', apiRoutes)
 router.use('/dashboard', dashboardRoutes)
-router.use('/login', loginRoutes)
 
 module.exports = router
