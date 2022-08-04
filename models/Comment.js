9da4bc85c8ab941f14ee2cde82f54d40420b2198
hmanjun <harshith.hm03@gmail.com>
2022-08-03 19:33:45 -0700

    Add Comment model and update Post model to use prebuilt datestamps

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,9 @@
 const {Model, DataTypes} = require('sequelize')
 const sequelize = require('../config/connection')
 
-class Post extends Model {}
+class Comment extends Model {}
 
-Post.init({
+Comment.init({
     id: {
         type: DataTypes.INTEGER,
         allowNull: false,
@@ -18,21 +18,16 @@ Post.init({
             key: 'id'
         }
     },
-    title: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
     body: {
         type: DataTypes.TEXT,
-        allowNull: false,
+        allowNull: false
     }
 },
 {
     sequelize,
     timestamps: true,
-    freezeTableName: true,
     underscored: true,
-    modelName: 'post'
+    modelName: 'comment'
 })
 
-module.exports = Post
+module.exports = Comment
