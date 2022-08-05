9d966fceda5edb98279d600a9d774999b2a17d70
hmanjun <harshith.hm03@gmail.com>
2022-08-04 19:24:05 -0700

    Add homepage file and get request listener

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,23 @@
 const router = require('express').Router()
+const {Post,Comment,User} = require('../models')
+
+router.get('/', async (req,res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [{model: User, attributes: ['username']}]
+        })
+
+        const posts = postData.map((post) => post.get({plain: true}))
+        //res.status(200).json(posts)
+        res.render('homepage', {
+            posts, logged_in: req.session.logged_in
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+
+
 
 module.exports = router
